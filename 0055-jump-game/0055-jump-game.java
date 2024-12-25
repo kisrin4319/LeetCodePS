ceadaea5cdff30d79346df3354e99e16b533b7d9
Mingi Jeon <kisrin4319@gmail.com>
2024-12-25 23:17:33 +0900

    Time: 1 ms (100%), Space: 45.9 MB (32.56%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,40 +1,11 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
 class Solution {
-    static boolean[] isJumped;
     public boolean canJump(int[] nums) {
-
-        isJumped = new boolean[nums.length];
-
-        Queue<Position> q = new LinkedList<>();
-        
-        int index = 0;
-
-        q.add(new Position(index,nums[index]));
-        isJumped[index] = true;
-
-        while (!q.isEmpty()) {
-            Position p = q.poll();
-            int px = p.index;
-            int jx = p.index + p.jump;
-
-            for (int i = px; i <= jx; i++){
-                if (i<nums.length && !isJumped[i]){
-                    q.add(new Position(i,nums[i]));
-                    isJumped[i] = true;
-                }
+        int position = nums.length-1;
+        for(int index = position-1; index >= 0; index--) {
+            if (nums[index] + index >= position) {
+                position = index;
             }
         }
-        return isJumped[nums.length-1];
-    }
-}
-class Position {
-    int index;
-    int jump;
-
-    Position(int index, int jump) {
-        this.index = index;
-        this.jump = jump;
+        return position == 0;
     }
 }
