30ea3d1a176d76a5c7aa706cca5264fcfc20a6ec
Mingi Jeon <kisrin4319@gmail.com>
2024-12-30 16:54:44 +0900

    Time: 3 ms (72.51%), Space: 43.2 MB (58.7%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,14 +1,3 @@
-import java.util.HashMap;
-
-public class Main {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-
-        System.out.println(solution.isPalindrome(" "));
-    }
-}
-
-
 class Solution {
     public boolean isPalindrome(String s) {
 
