db1c34207f1cb54464f11ca9b96d83f1f3488aea
Mingi Jeon <kisrin4319@gmail.com>
2024-12-30 16:42:09 +0900

    Time: 3 ms (72.51%), Space: 43.2 MB (58.7%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+
+        s = s.toLowerCase();
+        int start = 0;
+        int end = s.length() - 1;
+        
+        boolean result = true;
+
+        while(start < end) {
+            if (!isAlphanumeric(s.charAt(start))) {
+                start++;
+                continue;
+            }
+
+            if (!isAlphanumeric(s.charAt(end))) {
+                end--;
+                continue;
+            }
+
+            if (s.charAt(start) == s.charAt(end)) {
+                start++;
+                end--;
+            } else {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+    boolean isAlphanumeric(char c) {
+        return c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z' || c >= '0' && c <= '9';
+    }
+}
