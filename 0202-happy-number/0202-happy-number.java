1ad82a418e47afd38f97fa4245a3a3468105174e
Mingi Jeon <kisrin4319@gmail.com>
2025-01-18 03:01:43 +0900

    Time: 1 ms (70.61%), Space: 41.1 MB (29.11%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,23 +1,26 @@
+import java.util.HashSet;
+
 class Solution {
     public boolean isHappy(int n) {
+        HashSet<Integer> seen = new HashSet<>();
 
-        HashSet<Integer> hs = new HashSet<>();
-
-        while(n!=1) {
-            int sum  = 0;
-            int temp = n;
-
-            while(temp !=0) {
-                int r = temp % 10;
-                temp = temp/10;
-                sum+= r*r;
-            }
-            if (hs.contains(sum)) {
+        while (n != 1) {
+            if (seen.contains(n)) {
                 return false;
             }
-            hs.add(sum);
-            n = sum;
+            seen.add(n);
+            n = getNext(n);
         }
         return true;
+    }
+
+    private int getNext(int n) {
+        int sum = 0;
+        while (n != 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
     }
 }
