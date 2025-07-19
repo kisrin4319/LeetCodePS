5d3745b41d2823464bbdce1be12b844779b1dab7
Mingi Jeon <kisrin4319@gmail.com>
2025-07-20 00:21:41 +0900

    Time: 2 ms (80.4%), Space: 46.1 MB (66.72%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,19 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
 
-        int index = nums.length - 1;
+        int index = 0;
 
-        //뒤에서부터 채워지는 0의 위치
-        int zeroPoint = nums.length - 1;
-
-        while(index >= 0) {
-            if (nums[index] == 0) {
-                for (int i = index; i < zeroPoint; i++) {
-                    nums[i] = nums[i+1];
-                }
-                nums[zeroPoint--] = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+              nums[index] = nums[i];
+              index++;
             }
-            index--;
+        }
+        for (int i = index; i < nums.length; i++) {
+            nums[i] = 0;
         }
     }
 }
