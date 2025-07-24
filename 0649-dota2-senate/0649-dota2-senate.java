c90a32ddeab6b06bb7d2aff1615859ee2e05d0e8
Mingi Jeon <kisrin4319@gmail.com>
2025-07-24 18:26:13 +0900

    Time: 11 ms (73.7%), Space: 46 MB (29.81%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.java
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -1,18 +1,31 @@
 class Solution {
     public String predictPartyVictory(String senate) {
+        Queue<Integer> radiant = new LinkedList<>();
+        Queue<Integer> dire = new LinkedList<>();
+        int n = senate.length();
 
-        Queue<Character> q = new LinkedList<>();
-        for(char c : senate.toCharArray()) {
-            q.offer(c);
+        // 각 진영의 인덱스를 큐에 저장
+        for (int i = 0; i < n; i++) {
+            if (senate.charAt(i) == 'R') {
+                radiant.offer(i);
+            } else {
+                dire.offer(i);
+            }
         }
-        while(q.size() != 1 ) {
-            char key =  q.poll();
-            char removeElement = key == 'R' ? 'D' : 'R';
-            if (!q.remove(removeElement)) {
-                return key ==  'R' ? "Radiant" : "Dire";
-            };
-            q.offer(key);
+
+        // 큐가 빌 때까지 반복
+        while (!radiant.isEmpty() && !dire.isEmpty()) {
+            int rIndex = radiant.poll();
+            int dIndex = dire.poll();
+
+            // 인덱스가 작은 쪽이 상대를 밴하고 살아남아 큐에 다시 들어감
+            if (rIndex < dIndex) {
+                radiant.offer(rIndex + n); // 다음 라운드를 위해 순환 위치 추가
+            } else {
+                dire.offer(dIndex + n);
+            }
         }
-        return q.peek() == 'R' ? "Radiant" : "Dire";
+
+        return radiant.isEmpty() ? "Dire" : "Radiant";
     }
 }
